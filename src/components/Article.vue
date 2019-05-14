d32994049399df078741af4be614a62b9b45c964
MacBook <macbookpro@PIxels-MacBook-Pro.local>
2019-05-14 01:14:24 -0400

    vue.js challenge

---
+++ src/components/Article.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        this is an article with the id of {{ articleId }}
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Article',
+        props: ["articleId"]
+    }
+</script>
+<style>
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header class="header">
+        <router-link to="/">
+            <Logo />
+        </router-link>
+        <div id="nav">
+            <router-link to="/hacker-news">Hacker News</router-link> |
+            <router-link to="/random-cat">Random Kitty</router-link>
+        </div>
+    </header>
+</template>
+<script>
+    import Logo from './Logo'
+    export default {
+        name: "Header",
+        components: {
+            Logo
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .header{
+        color: green;
+        text-align: center;
+        padding: 10px;
+        
+        a{
+            color: green;
+            padding-right: 5px;
+            text-decoration: none;
+        }
+        a:visited{
+            color: green;
+        }
+    }
+  
+</style>
